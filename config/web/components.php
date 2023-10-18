9734d6f86f5985aa0f0b8d151e75303a888dc699
Wilmer Arambula <terabytesoftw@gmail.com>
2023-10-18 11:32:30 -0300

    Fix config.

--- config/web/components.php
+++ config/web/components.php
@@ -2,16 +2,11 @@
 
 declare(strict_types=1);
 
-use yii\db\Connection;
 use yii\i18n\PhpMessageSource;
 use Yii\User\Model\Identity;
 
 return [
     'components' => [
-        'db' => [
-            'class' => Connection::class,
-            'dsn' => 'sqlite:yiitest.sq3',
-        ],
         'i18n' => [
             'translations' => [
                 'yii.user' => [
