e26e77e2cc4a7703e9277325ca1d1be8ba4ad4df
Wilmer Arambula <terabytesoftw@gmail.com>
2023-12-03 08:23:28 -0300

    Add guest menu items to app.params.

--- config/tests/params-web.php
+++ config/tests/params-web.php
@@ -11,6 +11,20 @@ return [
         ],
     ],
     'app.params' => [
+        'app.menu.isguest' => [
+            [
+                'label' => 'Register',
+                'url' => ['/register/index'],
+                'order' => 3,
+                'category' => 'yii.user',
+            ],
+            [
+                'label' => 'Login',
+                'url' => ['/login/index'],
+                'order' => 4,
+                'category' => 'yii.user',
+            ],
+        ],
         'icons' => '@npm/fortawesome--fontawesome-free/svgs/{family}/{name}.svg',
     ],
 ];
