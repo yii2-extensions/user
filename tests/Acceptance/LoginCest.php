ef1fdf23fd721375d94ff8a93150f036c2b4629b
Wilmer Arambula <terabytesoftw@gmail.com>
2023-12-08 16:06:59 -0300

    Add successWithEmail method to LoginCest.

--- tests/Acceptance/LoginCest.php
+++ tests/Acceptance/LoginCest.php
@@ -84,7 +84,30 @@ final class LoginCest
         $I->accountConfirmation(false);
     }
 
-    public function success(AcceptanceTester $I): void
+    public function successWithEmail(AcceptanceTester $I): void
+    {
+        $I->wantTo('security login username submit form success data.');
+        $I->haveFixtures([Account::class => AccountFixture::class]);
+        $account = $I->grabFixture(Account::class, 'confirmed');
+
+        $I->amGoingTo('navigate to the login page.');
+        $I->amOnRoute('login/index');
+
+        $I->wantTo('ensure that login page works.');
+        $I->expectTo('see page index.');
+        $I->see(Yii::t('yii.user', 'Sign in'), 'h1');
+        $I->see(Yii::t('yii.user', 'Please fill out the following fields to Sign in.'));
+
+        $I->expectTo('fill in the form.');
+        $I->fillField('#loginform-login', $account->email);
+        $I->fillField('#loginform-password', '123456');
+        $I->click(Yii::t('yii.user', 'Sign in'));
+
+        $I->expectTo('see that the user is logged in.');
+        $I->seeLink(Yii::t('yii.user', 'Logout'));
+    }
+
+    public function successWithUsername(AcceptanceTester $I): void
     {
         $I->wantTo('security login username submit form success data.');
         $I->haveFixtures([Account::class => AccountFixture::class]);
